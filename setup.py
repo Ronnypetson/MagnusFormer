dfdc86e01d7fcde472b560f2677ddb2b53811da1
Ronnypetson Souza da Silva <rsronnypetson4@gmail.com>
2022-04-25 20:50:20 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Transformer-based autoregressive language model for chess PGN generation resembling human games.',
+    author='Ronnypetson Souza da Silva',
+    license='MIT',
+)
